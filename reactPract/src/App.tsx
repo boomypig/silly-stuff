190970632ecf94df0601230a4c03a4cf453dbb96
Bryan Lopez <brainalexis119@gmail.com>
2026-01-22 19:37:17 -0700

    components usestates dynamics AAAAAAAAAAAH

--- reactPract/src/App.tsx
+++ reactPract/src/App.tsx
@@ -1,20 +1,29 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 function App() {
   let items = ["new york", "los angeles", "portland", "tokyo"];
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+  const [alertVisible, setAlertVisiblity] = useState(false);
   return (
     <div>
       <ListGroup
         items={items}
-        heading="Cities"
+        heading={"Cities"}
         onSelectItem={handleSelectItem}
       />
-      <Alert>
-        Hello World <span>ZA WORLD</span>
-      </Alert>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisiblity(false)}>
+          Hello World <span>ZA WORLD</span>
+        </Alert>
+      )}
+
+      <Button color="primary" onClick={() => setAlertVisiblity(true)}>
+        My button
+      </Button>
     </div>
   );
 }
